b374df07e97fb9007dd2ad7e4a6682e6f1dcbd13
michelle nderi <nderimichelle@gmail.com>
2023-04-26 20:25:46 +0300

    Made App listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      setListings(data);
+    })
+    .catch(error => {
+      console.error('Error fetching transactions:', error);
+    });
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
