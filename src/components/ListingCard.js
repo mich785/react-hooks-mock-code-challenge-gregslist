afbf963edfbb59542e9d2f1d73a311cb800dc506
michelle nderi <nderimichelle@gmail.com>
2023-04-26 20:45:21 +0300

    Handled Favourite

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard({listing}) {
+function ListingCard({ listing, onDelete }) {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
+
+
   return (
     <li className="card">
       <div className="image">
-        <span className="price">$0</span>
+        <span className="price">${listing.price}</span>
         <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFavorited ? (
+          <button
+            className="emoji-button favorite active"
+            onClick={handleFavorite}
+          >
+            ★
+          </button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button
+            className="emoji-button favorite"
+            onClick={handleFavorite}
+          >
+            ☆
+          </button>
         )}
         <strong>{listing.description}</strong>
         <span> · {listing.location}</span>
-        <button className="emoji-button delete">🗑</button>
+        <button
+          className="emoji-button delete"
+        >
+          🗑
+        </button>
       </div>
     </li>
   );
